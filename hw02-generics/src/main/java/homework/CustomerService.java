a428305be1973133d31ed12d939bc822b1ae1d69
Maxim <maksfomin@gmail.com>
2023-07-18 23:00:09 +0300

    Issue #2 * Refactored CustomerService

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -1,57 +1,32 @@
 package homework;
 
-import java.util.*;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 import static java.util.Comparator.comparing;
-import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
 public class CustomerService {
 
     private final NavigableMap<Customer, String> customers;
 
-    private final Set<Customer> backupCustomers;
-
     public CustomerService() {
         this.customers = new TreeMap<>(comparing(Customer::getScores));
-        this.backupCustomers = new HashSet<>();
     }
 
     public Map.Entry<Customer, String> getSmallest() {
-        Map.Entry<Customer, String> firstCustomer = customers.firstEntry();
-        checkCustomerName(firstCustomer);
-        return firstCustomer;
+        Map.Entry<Customer, String> first = customers.firstEntry();
+        return Map.entry(new Customer(first.getKey().getId(), first.getKey().getName(), first.getKey().getScores()), first.getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        Map.Entry<Customer, String> nextCustomer = customers.higherEntry(customer);
-        return checkCustomerId(customer, nextCustomer);
+        Map.Entry<Customer, String> customerNext = customers.higherEntry(customer);
+        Map.Entry<Customer, String> customerLast = customers.lastEntry();
+        return customerNext == null && customer.getId() <= customerLast.getKey().getId() ? customerLast : customerNext;
     }
 
     public void add(Customer customer, String data) {
-        backupCustomers.add(new Customer(customer.getId(), customer.getName(), customer.getScores()));
         customers.put(customer, data);
-    }
-
-    private void checkCustomerName(Map.Entry<Customer, String> firstCustomer) {
-        if (firstCustomer != null && isNotEmpty(backupCustomers)) {
-            Customer backupCustomer = backupCustomers.stream()
-                    .filter(c -> c.getId() == firstCustomer.getKey().getId())
-                    .findFirst()
-                    .orElse(null);
-            if (backupCustomer != null && !firstCustomer.getKey().getName().equals(backupCustomer.getName())) {
-                firstCustomer.getKey().setName(backupCustomer.getName());
-            }
-        }
-    }
-
-    private Map.Entry<Customer, String> checkCustomerId(Customer customer, Map.Entry<Customer, String> nextCustomer) {
-        if (nextCustomer == null && isNotEmpty(backupCustomers)) {
-            if (backupCustomers.stream().anyMatch(c -> c.getId() == customer.getId())) {
-                return customers.lastEntry();
-            }
-            return null;
-        }
-        return nextCustomer;
     }
 
 }
