7d16075c7ecd286d78093c37dbf371a5e4617e5f
Maxim <maksfomin@gmail.com>
2023-08-26 00:09:56 +0300

    Added new content

--- hw07-structuralPatterns/build.gradle.kts
+++ hw07-structuralPatterns/build.gradle.kts
@@ -3,6 +3,12 @@ plugins {
 }
 
 dependencies {
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
     implementation("org.apache.commons:commons-lang3")
     implementation("org.apache.commons:commons-collections4")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
